71cdf91d8446106263497082bb36ff037d56f24f
Naoto Koshikawa <koshikawa@ppworks.jp>
2016-02-08 18:33:12 +0900

    Fix path

--- lib/enhanced_mysql_tasks.rb
+++ lib/enhanced_mysql_tasks.rb
@@ -4,9 +4,9 @@ module EnhancedMySQLTasks
 end
 
 if Gem::Version.create(ActiveRecord.version) >= Gem::Version.create('4.2.0')
-  require 'enhanced_mysql_tasks/Rails42/mysql_database_tasks'
+  require 'enhanced_mysql_tasks/rails42/mysql_database_tasks'
   ActiveRecord::Tasks::MySQLDatabaseTasks.__send__(:prepend, EnhancedMySQLTasks::Rails42::MySQLDatabaseTasks)
 elsif Gem::Version.create(ActiveRecord.version) >= Gem::Version.create('4.1.0')
-  require 'enhanced_mysql_tasks/Rails41/mysql_database_tasks'
+  require 'enhanced_mysql_tasks/rails41/mysql_database_tasks'
   ActiveRecord::Tasks::MySQLDatabaseTasks.__send__(:prepend, EnhancedMySQLTasks::Rails41::MySQLDatabaseTasks)
 end
