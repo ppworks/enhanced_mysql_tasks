ccc4d8e840f3ee2988630d45c5e590c8e889d1a7
Naoto Koshikawa <koshikawa@ppworks.jp>
2016-02-08 16:59:22 +0900

    All green

--- test/enhanced_mysql_tasks_test.rb
+++ test/enhanced_mysql_tasks_test.rb
@@ -4,8 +4,4 @@ class EnhancedMysqlTasksTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::EnhancedMysqlTasks::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
